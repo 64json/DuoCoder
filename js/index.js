1305df0578b9437b70f1f4d0a3fca72851fff0fa
Jason Park <parkjs814@gmail.com>
2016-10-27 09:44:05 -0500

    animations

--- js/index.js
+++ js/index.js
@@ -11,4 +11,25 @@ $(document).ready(() => {
   };
   onResize();
   $(window).resize(onResize);
+
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+
+  const $unfocused = $('.focused');
+  const $lang_to = $('#lang-to');
+  const onScroll = () => {
+    $unfocused.toggleClass('unfocused', $(document).scrollTop() <= 168);
+  };
+  onScroll();
+  $(window).scroll(onScroll);
 });
