f83aaf341c904f62f8f3dd3469d8f4d3a0300f15
Jason Park <parkjs814@gmail.com>
2016-10-27 09:00:09 -0500

    main complete

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+  const $stick_to_bottom_fake = $('.stick-to-bottom.fake');
+  const $stick_to_bottom_real = $('.stick-to-bottom.real');
+  const $lang_from = $('#lang-from');
+  const $fullscreen = $('.fullscreen');
+  const onResize = () => {
+    const top = $lang_from.offset().top + $lang_from.height();
+    const bottom = $stick_to_bottom_fake.offset().top;
+    $fullscreen.toggleClass('full', top < bottom);
+    $stick_to_bottom_real.toggleClass('stick', top < bottom)
+  };
+  onResize();
+  $(window).resize(onResize);
+});
