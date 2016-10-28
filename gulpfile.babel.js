2b84a8301967d88a6b6fa26d0385108c467c371e
Jason Park <parkjs814@gmail.com>
2016-10-28 16:35:15 -0500

    prevent gulp dying

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,7 +4,6 @@ import path from "path";
 import gulp from "gulp";
 import uglify from "gulp-uglify";
 import autoprefixer from "gulp-autoprefixer";
-import concat from "gulp-concat";
 import header from "gulp-header";
 import gutil from "gulp-util";
 import sourcemaps from "gulp-sourcemaps";
@@ -42,13 +41,18 @@ const cssDir = path.join(__dirname, 'css', '**', '*.scss');
 const jsDir = path.join(__dirname, 'js', '**', '*.js');
 const pugDir = path.join(__dirname, 'pug', '**', '*.pug');
 
+function onError(err) {
+  console.log(err);
+  this.emit('end');
+}
+
 // CSS
 gulp.task('build-css', () => {
   gutil.log('\n\nBuild CSS Paths: \n', cssDir, '\n\n');
 
   return gulp.src(cssDir)
     .pipe(autoprefixer('last 2 version', 'ie 8', 'ie 9'))
-    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(header(banner, {pkg}))
     .pipe(gulp.dest(outputPaths.css))
     .pipe(connect.reload());
@@ -66,6 +70,7 @@ gulp.task('build-js', () => {
       presets: ['es2015']
     })
     .bundle()
+    .on('error', onError)
     .pipe(source(`${appName}.js`))
     .pipe(header(banner, {pkg}))
     .pipe(buffer())
@@ -84,7 +89,7 @@ gulp.task('build-pug', () => {
   var PUG_LOCALS = {title: pkg.name};
 
   return gulp.src(pugDir)
-    .pipe(pug({locals: PUG_LOCALS}))
+    .pipe(pug({locals: PUG_LOCALS}).on('error', onError))
     .pipe(gulp.dest(outputPaths.pug))
     .pipe(connect.reload());
 });
