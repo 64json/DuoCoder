9059701447d8f4f087df4f24b087a7bcb2a0ef2c
Jason Park <parkjs814@gmail.com>
2016-10-30 04:02:33 -0500

    submit lang selected

--- js/dom/setup_main/setup_languages.js
+++ js/dom/setup_main/setup_languages.js
@@ -6,12 +6,28 @@ module.exports = () => {
   for (const container of ['#lang-from', '#lang-to']) {
     const $container = $(container);
     for (const language of languages) {
-      const $lang = $(`<a class="lang" href="${container == '#lang-from' ? '#dots' : './learn.html'}"></a>`);
+      const $lang = $(`<a class="lang" href="${container == '#lang-from' ? '#dots' : '#top'}"></a>`);
       $container.append($lang);
       const $logo = $(`<div class="logo" style="background-image: url('./images/lang_${language.ext}.png')"></div>`);
       $lang.append($logo);
       $logo.append(`<div class="logo mono" style="background-image: url('./images/lang_${language.ext}_mono.png')"></div>`);
       $lang.append(`<div class="name">${language.name}</div>`);
     }
+    $container.find('.lang').click(function () {
+      $container.find('.lang.selected').removeClass('selected');
+      $(this).addClass('selected');
+    });
   }
+  $('.lang').click(() => {
+    const $lang_to = $('#lang-to');
+    const $lang_from_selected = $('#lang-from .lang.selected');
+    const $lang_to_selected = $('#lang-to .lang.selected');
+    if ($lang_from_selected.length && $lang_to_selected.length) {
+      const from_index = $lang_from_selected.index();
+      const to_index = $lang_to_selected.index() - 2;
+      $lang_to.find('[name="lang_from"]').val(languages[from_index].ext);
+      $lang_to.find('[name="lang_to"]').val(languages[to_index].ext);
+      $lang_to.submit();
+    }
+  });
 };
