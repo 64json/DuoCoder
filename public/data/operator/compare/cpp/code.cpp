9d930dcbaa8299e00d66165e4f3441b141a265ae
Jason Park <parkjs814@gmail.com>
2016-12-26 16:09:44 +0900

    add comparisons category of cpp and py

---
+++ public/data/operator/compare/cpp/code.cpp
@@ -0,0 +1,8 @@
+int a = 2;
+int b = 5;
+int eq = #0{[a == b]};  // false
+int ne = #1{[a != b]};  // true
+int lt = #2{[a < b]};  // true
+int gt = #3{[a > b]};  // false
+int lte = #4{[a <= b]};  // true
+int gte = #5{[a >= b]};  // false
---
+++ public/data/operator/compare/py/code.py
@@ -0,0 +1,8 @@
+a = 2
+b = 5
+eq = #0{[a == b]}  # False
+ne = #1{[a != b]}  # True
+lt = #2{[a < b]}  # True
+gt = #3{[a > b]}  # False
+lte = #4{[a <= b]}  # True
+gte = #5{[a >= b]}  # False
